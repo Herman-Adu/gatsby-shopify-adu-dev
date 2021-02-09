243d550e78b091dd394c9cb77a8248458991db92
Herman Adu <herman.adu@hotmail.com>
2021-02-09 16:10:45 +0000

    updated product update function and added axios to site

--- functions/product-update.js
+++ functions/product-update.js
@@ -1,14 +1,13 @@
 const faunadb = require('faunadb');
 const verifyWebhookIntegrity = require('shopify-verify-webhook');
+const axios = require('axios');
 
 const q = faunadb.query;
-
 const client = new faunadb.Client({
     secret: process.env.FAUNADB_SECRET,
 });
 
 exports.handler = function (event, context, callback) {
-
     const isValid = verifyWebhookIntegrity(
         process.env.SHOPIFY_WEBHOOK_KEY,
         event.headers['x-shopify-hmac-sha256'],
@@ -16,11 +15,52 @@ exports.handler = function (event, context, callback) {
     );
 
     if (isValid) {
-        callback(null, {
-            statusCode: 200,
-            body: "Hello world",
+        const body = JSON.parse(event.body);
+        const { id } = body;
+        delete body.updated_at;
+        body.variants.forEach(variant => {
+            delete variant.updated_at;
+            delete variant.inventory_quantity;
+            delete variant.old_inventory_quantity;
         });
-    } else {
+
+    const bodyString = JSON.stringify(body);
+
+    client
+        .query(q.Get(q.Match(q.Index('product_by_id'), id)))
+        .then(result => {
+            if (result.data.product !== bodyString) {
+            client
+                .query(
+                    q.Update(result.ref, {
+                        data: { product: bodyString },
+                    })
+                )
+                .then(() => {
+                    // call rebuild
+                    axios.post(process.env.NETLIFY_BUILD_URL);
+                })
+                .catch(e => {
+                    console.log('error updating product: ', e);
+                });
+            }
+        })
+        .catch(() => {
+            client
+                .query(
+                    q.Create(q.Collection('products'), {
+                        data: { id, product: bodyString },
+                    })
+                )
+                .then(() => {
+                    // call rebuild
+                    axios.post(process.env.NETLIFY_BUILD_URL);
+                })
+                .catch(e => {
+                    console.log('error adding to db: ', e);
+                });
+        });
+  } else {
         callback(null, {
             statusCode: 403,
             body: 'Error',
